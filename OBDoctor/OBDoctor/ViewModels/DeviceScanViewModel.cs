f94d14bf0d9138599eb732828a203c047cc96dfc
Daniel Christy <dchristy87@gmail.com>
2020-09-21 21:23:55 -0400

    BLE scanning and connection

---
+++ OBDoctor/OBDoctor/ViewModels/DeviceScanViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.EventArgs;
+using Plugin.BLE.Abstractions.Exceptions;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace OBDoctor.ViewModels {
+    public class DeviceScanViewModel : BaseViewModel {
+        private readonly IBluetoothLE bluetoothLE;
+        private readonly IAdapter adapter;
+
+        public Command ScanCommand => new Command(OnScanClicked);
+
+        private ObservableCollection<IDevice> devices;
+        public ObservableCollection<IDevice> Devices {
+            get => devices;
+            set {
+                devices = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private IDevice selectedDevice;
+        public IDevice SelectedDevice {
+            get => selectedDevice;
+            set {
+                selectedDevice = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DeviceScanViewModel() {
+            bluetoothLE = CrossBluetoothLE.Current;
+            adapter = bluetoothLE.Adapter;
+            Devices = new ObservableCollection<IDevice>();
+
+            adapter.ScanTimeout = 60000;
+            adapter.DeviceDiscovered += (sender, foundDevice) => OnDeviceDiscovered(foundDevice);
+        }
+
+        public async Task ConnectToDevice() {
+            try {
+                await adapter.ConnectToDeviceAsync(SelectedDevice);
+            } catch (DeviceConnectionException exception) {
+                Debug.WriteLine(exception);
+            }
+        }
+
+        private async void OnScanClicked() {
+            Devices.Clear();
+
+            var status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+            await adapter.StopScanningForDevicesAsync();
+            await adapter.StartScanningForDevicesAsync();
+        }
+
+        private void OnDeviceDiscovered(DeviceEventArgs deviceEventArgs) {
+            if (deviceEventArgs.Device != null && !string.IsNullOrEmpty(deviceEventArgs.Device.Name))
+                Devices.Add(deviceEventArgs.Device);
+        }
+    }
+}
---
+++ OBDoctor/OBDoctor/Views/DeviceScanPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using OBDoctor.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OBDoctor.Views {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DeviceScanPage : ContentPage {
+        public DeviceScanPage() {
+            InitializeComponent();
+        }
+
+        private async void DeviceList_ItemTapped(object sender, ItemTappedEventArgs e) {
+            var deviceScanViewModel = (DeviceScanViewModel)BindingContext;
+
+            bool answer = await DisplayAlert("Are you sure?", $"Would you like to connect to the device: {deviceScanViewModel.SelectedDevice.Name}", "Yes", "No");
+
+            if (answer) {
+                await deviceScanViewModel.ConnectToDevice();
+            }
+        }
+    }
+}
